c77dd59ecbc5287ab5428df65980f4f242091fd9
HT-Jasper <jasper.huytran@gmail.com>
2026-04-20 19:41:57 -0700

    Components, Hooks, State, Props

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,12 @@
-function TodoList() {
+import TodoListItem from './TodoListItem.jsx';
 
-    const todoList = [
-      {id: 1, title: "review resources"},
-      {id: 2, title: "take notes"},
-      {id: 3, title: "code out app"},
-    ]
+export default function TodoList({todoList}) {
 
     return (
         <ul>
-            {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+            {todoList.map(todo => (
+            <TodoListItem key={todo.id} todo={todo} />
+            ))}
         </ul>
     );
 }
-
-export default TodoList;
